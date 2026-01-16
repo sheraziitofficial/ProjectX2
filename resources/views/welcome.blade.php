cc4572bab199e163db27be5131a2fa92f3ed47c6
Sherazi IT <sheraziit.internal@gmail.com>
2026-01-17 01:53:46 +0600

    text x2 2nd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -831,7 +831,7 @@
 </head>
 
 <body class="antialiased">
-    <h1>Hey World</h1>
+    <h1>Hey World text x2</h1>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
